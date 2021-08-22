94034a9080d42dc88d7e12f80a64d5e710ffc12b
BoredTweak <alex.elia37@gmail.com>
2021-08-22 09:10:47 -0700

    Adding primary key

--- sql/V1__Create_Table.sql
+++ sql/V1__Create_Table.sql
@@ -2,5 +2,6 @@ CREATE TABLE toxicity_annotations (
     rev_id decimal,
     worker_id decimal,
     toxicity decimal,
-    toxicity_score decimal 
+    toxicity_score decimal,
+    PRIMARY KEY (rev_id, worker_id)
 );
